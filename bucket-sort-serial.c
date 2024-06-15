9df153c788876b0228eff125ef506196815daf58
Ian Rangel Passos <ianrangelpw@gmail.com>
2024-06-15 18:30:13 -0300

    update serial bucket sort

--- bucket-sort-serial.c
+++ bucket-sort-serial.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// Função para ordenar um bucket individual usando Insertion Sort
-void insertionSort(float bucket[], int size) {
-    for (int i = 1; i < size; ++i) {
-        float key = bucket[i];
+int limit = 100000;
+
+// Funï¿½ï¿½o para ordenar um bucket individual usando Insertion Sort
+void insertionSort(int bucket[], int size)
+{
+    for (int i = 1; i < size; ++i)
+    {
+        int key = bucket[i];
         int j = i - 1;
-        while (j >= 0 && bucket[j] > key) {
+        while (j >= 0 && bucket[j] > key)
+        {
             bucket[j + 1] = bucket[j];
             j--;
         }
@@ -14,51 +20,73 @@ void insertionSort(float bucket[], int size) {
     }
 }
 
-// Função para ordenar um array arr[] de tamanho n usando Bucket Sort
-void bucketSort(float arr[], int n) {
+// Funï¿½ï¿½o para ordenar um array arr[] de tamanho n usando Bucket Sort
+void bucketSort(int arr[], int n)
+{
     // 1) Criar n buckets vazios
-    float** b = (float**)malloc(n * sizeof(float*));
-    int* size = (int*)malloc(n * sizeof(int)); // Armazenar o tamanho de cada bucket
-    for (int i = 0; i < n; i++) {
-        b[i] = (float*)malloc(n * sizeof(float));
+    int **b = (int **)malloc(n * sizeof(int *));
+    int *size = (int *)malloc(n * sizeof(int)); // Armazenar o tamanho de cada bucket
+    for (int i = 0; i < n; i++)
+    {
+        b[i] = (int *)malloc(n * sizeof(int));
         size[i] = 0;
     }
 
     // 2) Colocar os elementos do array em buckets diferentes
-    for (int i = 0; i < n; i++) {
-        int bi = n * arr[i]; // Índice do bucket
+    for (int i = 0; i < n; i++)
+    {
+        // int bi = n * arr[i]; // ï¿½ndice do bucket
+        int bi = (int)((float)n * ((float)arr[i] / (float)limit));
         b[bi][size[bi]] = arr[i];
         size[bi]++;
     }
 
     // 3) Ordenar cada bucket individual usando Insertion Sort
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         insertionSort(b[i], size[i]);
     }
 
     // 4) Concatenar todos os buckets de volta em arr[]
     int index = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < size[i]; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < size[i]; j++)
+        {
             arr[index++] = b[i][j];
         }
-        free(b[i]); // Liberar a memória alocada para o bucket
+        free(b[i]); // Liberar a memï¿½ria alocada para o bucket
     }
-    free(b); // Liberar a memória alocada para os ponteiros dos buckets
-    free(size); // Liberar a memória alocada para os tamanhos dos buckets
+    free(b);    // Liberar a memï¿½ria alocada para os ponteiros dos buckets
+    free(size); // Liberar a memï¿½ria alocada para os tamanhos dos buckets
 }
 
-// Programa principal para testar a função acima
-int main() {
-    float arr[] = {0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bucketSort(arr, n);
+// Programa principal para testar a funï¿½ï¿½o acima
+int main()
+{
+    int A[10000];
+    int n = 10000;
+    int i = 0;
 
-    printf("Array ordenado é \n");
-    for (int i = 0; i < n; i++) {
-        printf("%f ", arr[i]);
+    srand(time(NULL));
+
+    for (i = 0; i < n; i++)
+    {
+        A[i] = rand() % limit;
     }
+
+    bucketSort(A, n);
+
+    printf("Array ordenado \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+
+        if (((i + 1) % 10) == 0)
+        {
+            printf("\n");
+        }
+    }
+
     return 0;
 }
-
-
