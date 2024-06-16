d391f47bd8a415087949a7d6c832c701f8aaf8d0
anaelporto <anaelporto@hotmail.com>
2024-06-16 15:50:47 -0300

    Code adjustments

--- bucket-sort-serial.c
+++ bucket-sort-serial.c
@@ -4,7 +4,7 @@
 
 int limit = 100000;
 
-// Fun��o para ordenar um bucket individual usando Insertion Sort
+// Ordenação com Insertion Sort
 void insertionSort(int bucket[], int size)
 {
     for (int i = 1; i < size; ++i)
@@ -20,10 +20,10 @@ void insertionSort(int bucket[], int size)
     }
 }
 
-// Fun��o para ordenar um array arr[] de tamanho n usando Bucket Sort
-void bucketSort(int arr[], int n)
+// Ordenando array A com Bucket Sort
+void bucketSort(int A[], int n)
 {
-    // 1) Criar n buckets vazios
+    // 1 - Criando N Buckets
     int **b = (int **)malloc(n * sizeof(int *));
     int *size = (int *)malloc(n * sizeof(int)); // Armazenar o tamanho de cada bucket
     for (int i = 0; i < n; i++)
@@ -32,36 +32,35 @@ void bucketSort(int arr[], int n)
         size[i] = 0;
     }
 
-    // 2) Colocar os elementos do array em buckets diferentes
+    // 2 - Colocando os elementos em diferentes buckets
     for (int i = 0; i < n; i++)
     {
-        // int bi = n * arr[i]; // �ndice do bucket
-        int bi = (int)((float)n * ((float)arr[i] / (float)limit));
-        b[bi][size[bi]] = arr[i];
+        int bi = (int)((float)n * ((float)A[i] / (float)limit));
+        b[bi][size[bi]] = A[i];
         size[bi]++;
     }
 
-    // 3) Ordenar cada bucket individual usando Insertion Sort
+    // 3 - Ordenando cada bucket com Insertion Sort
     for (int i = 0; i < n; i++)
     {
         insertionSort(b[i], size[i]);
     }
 
-    // 4) Concatenar todos os buckets de volta em arr[]
+    // 4 - Juntar os buckets novamente em A[]
     int index = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < size[i]; j++)
         {
-            arr[index++] = b[i][j];
+            A[index++] = b[i][j];
         }
-        free(b[i]); // Liberar a mem�ria alocada para o bucket
+        free(b[i]); // Liberando a memória alocada para o bucket
     }
-    free(b);    // Liberar a mem�ria alocada para os ponteiros dos buckets
-    free(size); // Liberar a mem�ria alocada para os tamanhos dos buckets
+    free(b);    // Liberando a memória alocada para os ponteiros dos buckets
+    free(size); // Liberando a memória alocada para os tamanhos dos buckets
 }
 
-// Programa principal para testar a fun��o acima
+// Programa principal para executar a ordenação
 int main()
 {
     int A[10000];
