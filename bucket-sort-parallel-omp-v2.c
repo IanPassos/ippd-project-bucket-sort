614edc1663b8d144d95661f0fdab868025829920
anaelporto <anaelporto@hotmail.com>
2024-06-18 22:33:00 -0300

    Best version achieved of Parallel Bucket Sort

--- bucket-sort-parallel-omp-v2.c
+++ bucket-sort-parallel-omp-v2.c
@@ -35,66 +35,51 @@ void bucketSort(float A[], int n, int limit)
         b[i] = (float *)malloc(n * sizeof(float)); //Alocando memória para os N possíveis itens
     }
 
-    #pragma omp parallel num_threads(threads) private(i, bi) shared(b, pos, A)
+    // 2 - Colocando os elementos em diferentes buckets
+    for (i = 0; i < n; i++)
     {
-        int my_id = omp_get_thread_num();
-        int num_threads = omp_get_num_threads();
-        int index_local = 0;
-        int n_per_thread = n / num_threads;
-        int start = my_id * n_per_thread;
-        int end = (my_id + 1) * n_per_thread;
+        bi = (int)((float)(n - 1) * (A[i] / (float)limit));
+        b[bi][pos[bi]] = A[i];
+        pos[bi] += 1;     
+    }
 
-        if (my_id == num_threads - 1) {
-            end = n;
+    // 3 - Ordenando cada bucket com Insertion Sort
+    #pragma omp parallel for
+    for (i = 0; i < n; i++)
+    {
+        if(pos[i] > 0){
+            insertionSort(b[i], pos[i]);
         }
-        
-        // 2 - Colocando os elementos em diferentes buckets
+    }
+    
+    // 4 - Juntar os buckets novamente em A[]    
+#pragma omp parallel
+{
+    int my_id = omp_get_thread_num();
+    int num_threads = omp_get_num_threads();
+    
+    int n_per_thread = n / num_threads;
+    int start = my_id * n_per_thread;
 
-        #pragma omp for schedule(static)
-        for (i = start; i < end; i++)
+    int index = 0;
+    int ii = 0;
+
+    for (ii = 0; ii < start; ii++)
+    {
+        index += pos[ii];
+    }
+
+    #pragma omp for
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < pos[i]; j++, index++)
         {
-            bi = (int)((float)(n - 1) * (A[i] / (float)limit));
-
-            #pragma omp critical
-            {
-                b[bi][pos[bi]] = A[i];
-                pos[bi] += 1;
-            }        
+            A[index] = b[i][j];
         }
-
-        #pragma omp barrier
-        
-        // 3 - Ordenando cada bucket com Insertion Sort
-        #pragma omp for
-        for (i = 0; i < n; i++)
-        {
-            if(pos[i] > 0){
-                insertionSort(b[i], pos[i]);
-            }
-        }
-
-        #pragma omp barrier
-        
-        // 4 - Juntar os buckets novamente em A[]
-
-        for (i = 0; i < start; i++)
-        {
-            index_local += pos[i];
-        }
-
-        for (i = start; i < end; i++)
-        {
-            for (j = 0; j < pos[i]; j++)
-            {
-                #pragma omp critical
-                {
-                    A[index_local++] = b[i][j];
-                }
-            }
-            free(b[i]);
-        }
-
-        #pragma omp barrier
+    }
+}
+    for(i = 0; i < n; i++){
+        free(b[i]);
     }
 
     free(b);    // Liberando a memória alocada para os ponteiros dos buckets
